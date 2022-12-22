0d27b7982c7e9de0ab9237a67419a62d03a9dd38
VictorGNetto <victornetto@discente.ufg.br>
2022-12-22 19:17:52 -0300

    Servidor multithreaded

--- chatclient.py
+++ chatclient.py
@@ -1,30 +1,41 @@
-from socket  import *
+from socket import *
 import sys
 import pickle
 import threading
-import const #- addresses, port numbers etc. (a rudimentary way to replace a proper naming service)
+# - addresses, port numbers etc. (a rudimentary way to replace a proper naming service)
+import const
+
 
 class RecvHandler(threading.Thread):
-  def __init__(self, sock):
-    threading.Thread.__init__(self)
-    self.client_socket = sock
+    def __init__(self, sock):
+        threading.Thread.__init__(self, daemon=True)
+        self.client_socket = sock
 
-  def run(self):
-    while True:
-        #print('Client receiving handler is ready.')
-        (conn, addr) = self.client_socket.accept() # accepts connection from server
-        #print('Server connected to me.')
-        marshaled_msg_pack = conn.recv(1024)   # receive data from server
-        msg_pack = pickle.loads(marshaled_msg_pack) # unmarshal message pack
-        print("MESSAGE: " + msg_pack[0] + " - FROM: " + msg_pack[1])
-        conn.send(pickle.dumps("ACK")) # simply send the server an Ack to confirm
-        conn.close()
-    return # We need a condition for graceful termination
+    def run(self):
+        while True:
+            # print('Client receiving handler is ready.')
+            # accepts connection from server
+            (conn, addr) = self.client_socket.accept()
+            # print('Server connected to me.')
+            marshaled_msg_pack = conn.recv(1024)   # receive data from server
+            # unmarshal message pack
+            msg_pack = pickle.loads(marshaled_msg_pack)
+            print("MESSAGE: " + msg_pack[0] + " - FROM: " + msg_pack[1])
+            # simply send the server an Ack to confirm
+            conn.send(pickle.dumps("ACK"))
+            conn.close()
 
-me = str(sys.argv[1]) # User's name (as registered in the registry. E.g., Alice, Bob, ...)
-client_sock = socket(AF_INET, SOCK_STREAM) # socket for server to connect to this client
-my_ip = const.registry[me][0]   # If using a proper naming service, client should know its
-my_port = const.registry[me][1] # addresses (which it would register in the ns)
+        # self.client_socket.close()
+
+
+# User's name (as registered in the registry. E.g., Alice, Bob, ...)
+me = str(sys.argv[1])
+# socket for server to connect to this client
+client_sock = socket(AF_INET, SOCK_STREAM)
+# If using a proper naming service, client should know its
+my_ip = const.registry[me][0]
+# addresses (which it would register in the ns)
+my_port = const.registry[me][1]
 client_sock.bind((my_ip, my_port))
 client_sock.listen(5)
 #
@@ -34,9 +45,11 @@ recv_handler.start()
 #
 # Handle interactive loop
 while True:
-    server_sock = socket(AF_INET, SOCK_STREAM) # socket to connect to server
+    server_sock = socket(AF_INET, SOCK_STREAM)  # socket to connect to server
+
     dest = input("ENTER DESTINATION: ")
     msg = input("ENTER MESSAGE: ")
+
     #
     # Connect to server
     try:
@@ -54,6 +67,6 @@ while True:
     if reply != "ACK":
         print("Error: Server did not accept the message (dest does not exist?)")
     else:
-        #print("Received Ack from server")
+        # print("Received Ack from server")
         pass
     server_sock.close()
